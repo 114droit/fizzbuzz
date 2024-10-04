7289318b74f43e9b8aec917f9875c9b16f6c6b0c
144droit <indieohren@gmail.com>
2024-10-04 12:29:23 +0200

    added script to check number inputs

---
+++ zahl-pruefen.py
@@ -0,0 +1,7 @@
+zahl = int(input("Bitte gib eine Zahl ein!"))
+if zahl > 0:
+    print("Die Zahl ist positiv")
+elif zahl < 0:
+    print("Die Zahl ist negativ")
+elif zahl == 0:
+    print("Die Zahl ist null")
